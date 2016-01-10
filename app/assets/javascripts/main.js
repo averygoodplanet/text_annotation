a61225d0e26785e9e0e2728867ab9d89b20322cc
averygoodplanet <averygoodplanet@gmail.com>
2016-01-10 12:05:09 -0600

    add annotator-full.min.js; tags, filter by tags plugins

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,1 +1,14 @@
-alert('controller js loading');
+console.log('Firing up annotator');
+console.log('The RoutingError for annotator-full.in.map in rails is a Chrome Dev tools artifact.');
+jQuery(function ($) {
+    $('h1').annotator()
+                          .annotator('addPlugin', 'Tags')
+                          .annotator('addPlugin', 'Filter', {
+                            filters: [
+                              {
+                                label: 'Tag',
+                                property: 'tags'
+                              }
+                            ]
+                            });
+});
