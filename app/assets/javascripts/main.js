b9fbecab0db490ab889a2f2a29a8435c82afc9e9
averygoodplanet <averygoodplanet@gmail.com>
2016-01-11 21:56:13 -0600

    ability to redrawAllHighlights()

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,20 +1,48 @@
 $( document ).ready(function() {
+  // var annotations is set in index.html.haml
 
-  // test out highlighting
-  // light green #00ff00 LOCATION
-  // light blue #33ccff ORGANIZATION
-  // light orange #ff6600 PERSON
-  $('textarea').highlightTextarea({
-    ranges: [{
-      color: '#ADF0FF',
-      start: 0,
-      length: 10
-    }, {
-      color: '#FFFF00',
-      ranges: [[40,45], [50,55]]
-    }]
-  });
+  function rangesFromAnnotations() {
+    var ranges = []
+    var color = ''
 
+    $.each( annotations, function (i, ann) {
+
+      switch (ann['category']) {
+        case 'LOCATION':
+          color = '#00FF00';
+          break
+        case 'ORGANIZATION':
+          color = '#33CCFF';
+          break
+        case 'PERSON':
+          color = '#FF6600';
+          break
+      }
+
+      this_range = {
+        color: color,
+        start: ann['start'],
+        length: (ann['end'] - ann['start']) + 1
+      }
+
+      ranges.push(this_range)
+    });
+    return ranges;
+  }
+
+  function redrawAllHighlights() {
+    // this clears all highlights and seems to be required 
+    // after you have drawn highlights before
+    $('textarea').highlightTextarea('destroy');
+
+    // draw highlights
+    $('textarea').highlightTextarea({
+      ranges: rangesFromAnnotations()
+    });
+  }
+
+  redrawAllHighlights();
+  
   $('textarea').select(function() {
     /// get position etc of mouse-selected text
     info = $('textarea').textrange();
