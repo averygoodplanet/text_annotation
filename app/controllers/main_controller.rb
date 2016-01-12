17b631f91995d93cde1f40b0d6016cf4e14edcc1
averygoodplanet <averygoodplanet@gmail.com>
2016-01-11 19:47:42 -0600

    use readonly textarea plaintext instead of div with html <mark> tags

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -12,33 +12,6 @@ class MainController < ApplicationController
       return
     end
 
-    raw_text = file.full_text
-
-    html = ''
-    resume_raw_index = nil
-    data_id = -1
-    in_between_string = ''
-
-    # build up html from raw_text and annotations
-    annotations = (file.annotations || []).sort_by{|a| a[:start]}
-
-    annotations.each do | a |
-      selected_raw_text = a.string_within_text
-      category = a.category
-      data_id += 1
-
-      if resume_raw_index.present?
-        in_between_string = raw_text[resume_raw_index..a.start - 1]
-      end
-
-      html += in_between_string
-      # tooltip from http://webdesignerhut.com/pure-css-tooltips-with-html5-data-attribute/
-      html_segment = %[<mark data-id="#{data_id}" class="tooltip tooltip-top" data-tooltip="#{category}"] +
-                                    %[data-start="#{a.start}" data-end="#{a.end}">#{selected_raw_text}</mark>]    
-      html += html_segment
-
-      resume_raw_index = a.end + 1
-    end
-    @full_text_html = html
+    @raw_text = file.full_text
   end
 end
